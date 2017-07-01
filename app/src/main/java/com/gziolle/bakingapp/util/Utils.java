1b299e9f9888ef51567939e7e7fa906a5ce74d0f
Guilherme Ziolle <gziolle@gmail.com>
2017-07-01 17:01:34 -0300

    Added String formating methods

--- app/src/main/java/com/gziolle/bakingapp/util/Utils.java
+++ app/src/main/java/com/gziolle/bakingapp/util/Utils.java
@@ -22,12 +22,58 @@ public class Utils {
     public static final String ACTION_UPDATE_WIDGET = "com.gziolle.bakingapp.UPDATE_WIDGET";
 
     public static String formatIngredient(Context context, Ingredient ingredient) {
-        return ingredient.getQuantity() + " " + ingredient.getMeasure() +
-                " of " + ingredient.getIngredient();
+        StringBuilder sb = new StringBuilder();
+        double quantity = ingredient.getQuantity();
+        String quantityString;
+        int integerQuantity = (int) ingredient.getQuantity();
+        if (quantity - integerQuantity == 0) {
+            quantityString = Integer.toString(integerQuantity);
+        } else {
+            quantityString = Double.toString(quantity);
+        }
+
+        String readableMeasure = getReadableMeasure(ingredient.getMeasure(), quantity);
+
+        StringBuilder builder = new StringBuilder();
+        builder.append(quantityString + " ");
+        if (readableMeasure != "") {
+            builder.append(readableMeasure);
+        }
+        builder.append(ingredient.getIngredient());
+
+        return builder.toString();
     }
 
     public static String formatProgressString(Context context, int current, int total) {
         return String.format(context.getString(R.string.step_progress, current, total));
     }
 
+    public static String getReadableMeasure(String measure, double quantity) {
+        if ("G".equals(measure)) {
+            return "grams of ";
+        } else if ("K".equals(measure)) {
+            return "kg of ";
+        } else if ("TBLSP".equals(measure)) {
+            if (quantity > 1) {
+                return "tablespoons of ";
+            } else {
+                return "tablespoon of ";
+            }
+        } else if ("TSP".equals(measure)) {
+            if (quantity > 1) {
+                return "teaspoons of ";
+            } else {
+                return "teaspoon of ";
+            }
+        } else if ("CUP".equals(measure)) {
+            if (quantity > 1) {
+                return "cups of ";
+            } else {
+                return "cup of ";
+            }
+        } else if ("OZ".equals(measure)) {
+            return "oz. of ";
+        }
+        return "";
+    }
 }
